f9d96269f2952c9b4649b0297ef46498a90763be
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-04-27 13:10:43 +0300

    исправила ошибки

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,8 @@
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
   const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const mobileBackdrop = document.querySelector("[data-menu-backdrop]");
+  const dasktopNavigation = document.querySelector("[data-menu-daskt-nav]");
 
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
@@ -9,5 +11,22 @@
 
   mobileBtnClose.addEventListener('click', () => {
     mobileMenuRef.classList.toggle("is-open");
+  });
+
+
+    menuBtnRef.addEventListener("click", () => {
+    mobileBackdrop.classList.toggle("is-open-backdrop");
+  })
+
+  mobileBtnClose.addEventListener('click', () => {
+    mobileBackdrop.classList.toggle("is-open-backdrop");
+  });
+
+   menuBtnRef.addEventListener("click", () => {
+    dasktopNavigation.classList.toggle("is-open-nav");
+  })
+
+  mobileBtnClose.addEventListener('click', () => {
+    dasktopNavigation.classList.toggle("is-open-nav");
   });
 })()
